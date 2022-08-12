3a6e30ed71c1b3dc784f3e3b6d1f6e3ca16e8b60
jyoti <jyotidhage10@gmail.com>
2022-08-12 16:41:34 +0530

    Welcome to the line Comparison Computation Problems

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LineComparison2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Linecomparison1 linecomparison1 = new Linecomparison1();
+            linecomparison1.LengthOfOneline();
+           
+            LineComparison2 lineComparison2 = new LineComparison2();
+            lineComparison2.LengthOfTwoLines();
+
+            LineComparison3 lineComparison3 = new LineComparison3();
+            lineComparison3.CompareTwoLines();
+
+            LineComparison4 lineComparison4 = new LineComparison4();
+            lineComparison4.LineCompareEqualMethod();
+
+        }
+    }
+}
